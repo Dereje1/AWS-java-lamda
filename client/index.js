1dc06595ab12ff279ab0c282cb006458bfe07fad
Dereje <getahun.d@gmail.com>
2020-10-03 21:27:49 -0400

    More prinicipal validation

--- client/index.js
+++ client/index.js
@@ -38,13 +38,13 @@ const postLambda = async ({principal, rate, period }) => {
 
 
 const validateAndSubmit = () => {
-    const input = {principal: principalInput.value, rate: rateInput.value, period: periodInput.value }
+    const input = {principal: principalInput.value.replace(/[$,]/g,''), rate: rateInput.value, period: periodInput.value }
     const isMissing = Object.keys(input).some(val => !Boolean(input[val]))
     if(isMissing) {
         output.style.visibility = 'hidden';
         return null;
     }
-    if(input.principal < 1000 || input.principal > 1000000){
+    if(input.principal < 1000){
         principalInput.style.background = '#ff00002e'
         output.style.visibility = 'hidden';
         return null;
@@ -68,27 +68,42 @@ const validateAndSubmit = () => {
     return postLambda(input)
 }
 
+const toCurrency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+});
+
 const updateOutPut = ({interestPaid,payment,principal,rate,period}) => {
 
-    // Create our number formatter.
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    });
+    // Create our number formatter
   
     const html = `
-    <span class="output-style"> Principal: ${formatter.format(principal)}</span>
+    <span class="output-style"> Principal: ${toCurrency.format(principal)}</span>
     <span class="output-style"> Interest Rate: ${rate.toFixed(2)}%</span>
     <span class="output-style"> Period: ${period} years</span>
     <hr/>
-    <span class="output-style-results"> Monthly Payment: ${formatter.format(payment)}</span>
-    <span class="output-style-results"> Interest Paid: ${formatter.format(interestPaid)}</span>
+    <span class="output-style-results"> Monthly Payment: ${toCurrency.format(payment)}</span>
+    <span class="output-style-results"> Interest Paid: ${toCurrency.format(interestPaid)}</span>
     `
     output.innerHTML = html;
+}
+
+const validatePrinicipal = ({target:{value}}) => {
+    const replacedCurrency = Number(value.replace(/[$,]/g,''))
+    if(!replacedCurrency || replacedCurrency > 5000000) {
+        principalInput.value = value.slice(0,value.length-1)
+        return
+    };
+    const currencyFormat = toCurrency.format(replacedCurrency.toFixed(0))
+    principalInput.value = currencyFormat
 }
 
 submitButton.addEventListener('click',validateAndSubmit);
 
 [principalInput, rateInput, periodInput].forEach(inputType => {
-    inputType.addEventListener('input', () => inputType.style.background = 'none')
+    inputType.addEventListener('input', (e) => {
+        inputType.style.background = 'none'
+        if(inputType.id === 'principal') validatePrinicipal(e)
+    })
 })
